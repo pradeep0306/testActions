155b529b18284a1358d22a785ef78b074bac2b31
pradeep0306 <pradeeppenumatcha03@gmail.com>
2017-05-27 20:44:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// Copyright 2016, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the 'License');
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+process.env.DEBUG = 'actions-on-google:*';
+const App = require('actions-on-google').ApiAiApp;
+
+// [START YourAction]
+exports.testActions = (request, response) => {
+  const app = new App({request, response});
+  console.log('Request headers: ' + JSON.stringify(request.headers));
+  console.log('Request body: ' + JSON.stringify(request.body));
+
+  // Fulfill action business logic
+  function responseHandler (app) {
+    // Complete your fulfillment logic and send a response
+    app.ask('Hello, World!');
+  }
+
+  const actionMap = new Map();
+  actionMap.set('tell.fact', responseHandler);
+
+  app.handleRequest(actionMap);
+};
